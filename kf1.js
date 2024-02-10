ab5a375699d56b620dcab3f5f72d51ab16ff4c95
Alexander Gorham <agram@hotmail.co.uk>
2024-02-10 03:22:56 +0100

    Found function that works, now to understand it...

--- kf1.js
+++ kf1.js
@@ -53,6 +53,8 @@ class BoardSquare
 
     setPossibleMoves()
     {
+        const thisRoundPosMoves = [];
+
         console.log(this.nextMoves);
         console.log(this.nextMoves[0]);
         console.log(this.nextMoves[0][0]);
@@ -74,8 +76,18 @@ class BoardSquare
                     if (boardSquares[nextMoveRow][nextMoveCol])
                     {
                         const nextMovePos = boardSquares[nextMoveRow][nextMoveCol];
+                        thisRoundPosMoves.push(nextMovePos);
                         nextMovePos.square.textContent = "P";
                         nextMovePos.square.classList.add("clickable-square");
+
+                        if (nextMovePos.square.classList.contains("goal"))
+                        {
+                            console.log("Found Goal!");
+                        }
+                        else
+                        {
+
+                        }
                     }
               
                    
@@ -84,6 +96,13 @@ class BoardSquare
 
                
             }
+        }
+
+        console.log(thisRoundPosMoves);
+
+        for (let i = 0; i < thisRoundPosMoves.length; i++)
+        {
+           // thisRoundPosMoves[i].setPossibleMoves();
         }
 
     }
@@ -141,5 +160,86 @@ const newBoard = new Board();
 
 
 
-boardSquares[4][4].square.classList.add("knight");
-boardSquares[4][4].setPossibleMoves();
+//boardSquares[4][4].square.classList.add("knight");
+//boardSquares[4][4].setPossibleMoves();
+
+//const goalPosition = boardSquares[7][7];
+//goalPosition.square.classList.add("goal");
+
+function findShortestPath(start, goal)
+{
+    let queue = [[start]]; // Queue of paths
+    let visited = new Set(); //Set of visited nodes
+
+    while (queue.length > 0)
+    {
+        let path = queue.shift(); // Get the first path from the queue
+        let node = path[path.length - 1]; // Get the last node on the path
+
+        if (node.row === goal.row && node.column === goal.column)
+        {
+            return path; //Return path if the goal is reached
+        }
+
+        if (!visited.has(node)) // Check if th enode has not been visited yet
+        {
+            visited.add(node);
+
+        }
+
+        let neighbors = getKnightsMoves(node); // Get all possible moves
+
+        for (let neighbor of neighbors)
+        {
+            if (!visited.has(neighbor))
+            {
+                let newPath = path.slice(); // Create a new path
+                newPath.push(neighbor); // Add neighbor to the new path
+                queue.push(newPath); // Add the new path to the queue
+            }
+        }
+    }
+
+    return null;
+}
+
+function getKnightsMoves(square)
+{
+    // Calculate potential moves based on knight's movement rules
+    // and filter out moves that are outside the board or have been visited
+
+    let moves = [];
+    let potentialMoves = [
+            [square.row-2, square.column-1],
+            [square.row-2, square.column+1],
+            [square.row-1, square.column-2],
+            [square.row-1, square.column+2],
+            [square.row+1, square.column-2],
+            [square.row+1, square.column+2],
+            [square.row+2, square.column-1],
+            [square.row+2, square.column+1]
+    ];
+
+    for (let [row, col] of potentialMoves)
+    {
+        if (row >= 0 && row < 8
+            && col >= 0 && col < 8)
+            {
+                moves.push(boardSquares[row][col]);
+            }
+    }
+
+    return moves.filter(move => !move.visited); // Filter out visited moves
+}
+
+const start = boardSquares[0][0]; // Starting Square
+start.square.classList.add("knight");
+const goal = boardSquares[7][7]; // Goal square
+goal.square.classList.add("goal");
+const shortestPath = findShortestPath(start, goal);
+
+for (let i = 0; i < shortestPath.length; i++)
+{
+    shortestPath[i].square.classList.add("short-path");
+}
+console.log(shortestPath);
